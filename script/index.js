27a0e3b4068714baaee26bcdc24601fb15eecc79
Sarah Eja <ejasarah1@gmail.com>
2024-08-09 13:22:13 +0100

    portfolio website

---
+++ script/index.js
@@ -0,0 +1,11 @@
+function toggleDropdown() {
+    var navLinks = document.getElementById("navLinks");
+    if (navLinks.style.display === "block") {
+      navLinks.style.display = "none";
+    } else {
+      navLinks.style.display = "block";
+    }
+  }
+
+
+
